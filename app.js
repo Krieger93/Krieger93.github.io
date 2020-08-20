bce6069989f6a05ce728dfe982ef383156a7031d
Gustavo Jejer <gjjejer@gmail.com>
2020-08-19 22:19:21 -0300

    Correcciones de JS

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var i = 0;
-var txt = ["Desarrollo Web"]; /* el texto */
+var txt = "Desarrollo Web"; /* el texto */
 var speed = 50; /* La duracion en milisegundos */
 
 function typeWriter() {
   if (i < txt.length) {
-    document.getElementById("parrafo_principal").innerHTML += txt[0].charAt(i);
+    document.getElementById("parrafo_principal").innerHTML += txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
   }
