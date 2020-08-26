c9a74cfaa55003da9576d5dad60b5aba75534674
Gustavo Jejer <gjjejer@gmail.com>
2020-08-26 10:30:03 -0300

    cambios en la disposición de los objetos en la sección conoceme

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var i = 0;
 var txt = "Web Developer"; /* el texto */
 var speed = 50; /* La duracion en milisegundos */
-var txth1 = "Gustavo Jejer [Portafolio]";
 
 function typeWriter() {
   if (i < txt.length) {
@@ -10,5 +9,4 @@ function typeWriter() {
     setTimeout(typeWriter, speed);
   }
 }
-document.getElementById("texto_principal").innerHTML = txth1;
 typeWriter();
