d24df730520326b3e5bae25b949e49f1d31379d6
Gustavo Jejer <gjjejer@gmail.com>
2020-09-11 00:15:20 -0300

    Nueva estructura de sitio con grid y flexbox, cambio de JS por JQUERY en pagina principal para agregar luego las animaciones.

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-var i = 0;
-var txt = "Front-End Developer"; /* el texto */
-var speed = 50; /* La duracion en milisegundos */
-
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("parrafo_principal").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
+$("document").ready(() => {
+  // Mobile Menu Toggle Button jQuery
+  $("#menu-btn").click(function testing() {
+    $(".mobile-menu").toggle();
+  });
+  var i = 0;
+  /* el texto */
+  var txt = "Front-End Developer";
+  /* La duracion en milisegundos */
+  var speed = 50;
+  function typeWriter() {
+    if (i < txt.length) {
+      document.getElementById("parrafo_principal").innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
   }
-}
-typeWriter();
+  typeWriter();
+});
